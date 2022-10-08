6918fd4c39082dbee7fc40fe5691d8f57d61c98e
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-10-08 15:25:36 +0300

    Done.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,4 +1,5 @@
 class MembershipsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def create
         membership = Membership.create!(member_params)
@@ -9,5 +10,9 @@ class MembershipsController < ApplicationController
 
     def member_params
         params.permit(:charge, :gym_id, :client_id)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ApplicationRecord
     belongs_to :gym
     belongs_to :client
+
+
+    validates :client, uniqueness: { scope: :gym }
 end
