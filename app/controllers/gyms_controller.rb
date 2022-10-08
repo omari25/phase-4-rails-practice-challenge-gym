fd4d5e26e0e1c0821bb0c864ebb5f0d03b28f798
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-10-08 16:05:28 +0300

    Done.

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,6 +1,10 @@
 class GymsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+    def index
+        render json: Gym.all, status: :ok
+    end
+
     def show
         gym = Gym.find_by!(id: params[:id])
         render json: gym, status: :ok
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :memberships, only: [:create]
-  resources :gyms, only: [:show, :destroy]
-  resources :clients, only: [:show]
+  resources :gyms, only: [:index, :show, :destroy]
+  resources :clients, only: [:index, :show]
 end
